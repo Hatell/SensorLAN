70de361930015e4706c0a6bc7e1570d8a240fa6e
Harry Karvonen <harry.karvonen@gmail.com>
2013-01-22 21:43:44 +0200

    setup.py added

---
+++ SensorLAN/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# vi: et sw=2 fileencoding=utf8
+#
+
+from SensorLAN import SensorLAN
+from SensorXML import SensorXML
+from SensorGnuPG import SensorGnuPG
+from SensorSocket import SensorSocketUDP, SensorSocketEthernet
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# vi: et sw=2 fileencoding=utf8
+#
+
+from distutils.core import setup
+
+setup(
+  name = "SensorLAN",
+  description = "SensorLAN desc",
+  url = "http://github.com/Hatell/SensorLAN",
+  author = "Harry Karvonen",
+  author_email = "harry.karvonen@gmail.com",
+  packages = ["SensorLAN"],
+  data_files = [ "*.py" ]
+)
